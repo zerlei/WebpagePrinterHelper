bb7ac49b6181df3f98025ddea0456e7fdcb255c8
zerlei <zerlei@localhost.localdomain>
2025-04-16 20:45:17 +0800

    feat: add nix

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; };
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          bun
+        ];
+      };
+    };
+}
