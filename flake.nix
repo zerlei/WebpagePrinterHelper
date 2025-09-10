5aa865a3e788377ab9c8ae91be38de04aeae0527
zerlei <1445089819@qq.com>
2025-09-10 22:14:04 +0800

    CI/CD:  add github action

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
-
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           bun
